047f48774f516c3b2becc3f1dfc385329174202b
BJ Klayne B. Abordo <bjklayneabordo.fsd@gmail.com>
2025-01-16 13:10:10 +0800

    added Post Model

--- djangohtmx/core/models.py
+++ djangohtmx/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+defaultBody = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField(default=defaultBody)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=False)
+
+    class Meta:
+        ordering = ['-updated']
